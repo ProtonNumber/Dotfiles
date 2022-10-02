173133b2fc6700283820eb67a1a4174f38a6c96a
ProtonNumber <jwhinton@yenno.com>
2022-10-02 20:19:38 +0100

    updates

--- Emacs/init.el
+++ Emacs/init.el
@@ -164,3 +164,17 @@ by Prelude.")
 ;;; init.el ends here
 
 (xterm-mouse-mode 1)
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (if (derived-mode-p 'c-mode 'c++-mode)
+                (cppcm-reload-all)
+              )))
+;; OPTIONAL, somebody reported that they can use this package with Fortran
+(add-hook 'c90-mode-hook (lambda () (cppcm-reload-all)))
+;; OPTIONAL, avoid typing full path when starting gdb
+(global-set-key (kbd "C-c C-g")
+                '(lambda ()(interactive) (gud-gdb (concat "gdb --fullname " (cppcm-get-exe-path-current-buffer)))))
+;; OPTIONAL, some users need specify extra flags forwarded to compiler
+(setq cppcm-extra-preprocss-flags-from-user '("-I/usr/src/linux/include" "-DNDEBUG"))
+(setq shell-file-name "fish")
