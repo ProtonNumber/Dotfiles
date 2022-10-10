28db91ec043de42269c111015fdd319c4d319bea
ProtonNumber <jwhinton@yenno.com>
2022-10-10 12:54:24 +0100

    Modified snippets

--- Emacs/personal/custom.el
+++ Emacs/personal/custom.el
@@ -11,14 +11,16 @@
  '(org-startup-with-inline-images t)
  '(org-startup-with-latex-preview t)
  '(package-selected-packages
-   '(yasnippet-snippets yasnippet cdlatex cpputils-cmake cmake-ide cmake-mode haskell-mode yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))
+   '(yasnippet-lean yasnippet-snippets yasnippet cdlatex cpputils-cmake cmake-ide cmake-mode haskell-mode yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))
  '(warning-suppress-log-types
    '(((package reinitialization))
      ((package reinitialization))
      ((package reinitialization))))
  '(warning-suppress-types
    '(((package reinitialization))
-     ((package reinitialization)))))
+     ((package reinitialization))
+     ((package reinitialization))))
+ '(yas-global-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
